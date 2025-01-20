00a01b8d9434f2220440a2798c70f03b78705b02
Iury Silva de Almeida <iury.colombo@gmail.com>
2025-01-20 10:01:39 -0300

    subindo primeira parte do projeto

---
+++ src/main/java/br/com/iuryalmeida/TaskEase/TaskEaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.iuryalmeida.TaskEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iuryalmeida/TaskEase/TaskEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iuryalmeida.TaskEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
