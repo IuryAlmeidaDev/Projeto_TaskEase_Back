3a8359cd6c6f0283586bd4f31bf8ed1ca65cd391
IuryAlmeidaDev <iury.colombo@gmail.com>
2025-03-05 22:08:04 -0300

    permitindo acesso ao console h2

--- src/main/java/br/com/iuryalmeida/TaskEase/config/SecurityConfig.java
+++ src/main/java/br/com/iuryalmeida/TaskEase/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 public class SecurityConfig {
@@ -18,8 +19,13 @@ public class SecurityConfig {
                     .requestMatchers("/due/**").permitAll()
                     .requestMatchers("/swagger-ui/**").permitAll()
                     .requestMatchers("/v3/api-docs/**").permitAll()
+                    .requestMatchers("/h2-console/**").permitAll() // Permite o acesso ao console H2
                     .anyRequest().authenticated();
-            });
+            })
+            .headers(headers -> headers
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                    XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))); // Permite o uso de frames no mesmo domínio
+
         return http.build();
     }
 }
